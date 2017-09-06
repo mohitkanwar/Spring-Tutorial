d01b1cabcf548d3ca25dcb82064c4774877e048d
Mohit Kanwar <m.m.kanwar@gmail.com>
2017-09-06 19:26:16 +0530

    3-Web App

--- src/test/java/com/mk/spring/demo/WebLayerTest.java
+++ src/test/java/com/mk/spring/demo/WebLayerTest.java
@@ -22,7 +22,27 @@ public class WebLayerTest {
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello World")));
     }
+
+    @Test
+    public void shouldReturnDefaultGreeting() throws Exception {
+        this.mockMvc.perform(get("/greeting"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World!")));
+    }
+
+    @Test
+    public void shouldReturnNamedGreeting() throws Exception {
+        this.mockMvc.perform(get("/greeting?name=Test"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, Test!")));
+    }
 }
+
+
