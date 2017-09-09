fdc4650cc2941a9c6a10860aaebfe8d2bf2fed8f
Mohit Kanwar <m.m.kanwar@gmail.com>
2017-09-09 23:34:57 +0530

    styles to pages

--- src/main/java/com/mk/spring/demo/GreetingController.java
+++ src/main/java/com/mk/spring/demo/GreetingController.java
@@ -3,15 +3,19 @@ package com.mk.spring.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @RequestMapping(value = {"/","/greeting"})
+
+    public String greeting() {
         return "greeting";
     }
-
 }
