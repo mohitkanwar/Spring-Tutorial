b2616daa0a9fdbc54e9441eab670cc0617a8c623
Mohit Kanwar <m.m.kanwar@gmail.com>
2017-10-25 14:08:55 +0530

    using params in methods

--- src/main/java/com/mk/spring/demo/BookingAspect.java
+++ src/main/java/com/mk/spring/demo/BookingAspect.java
@@ -1,10 +1,13 @@
 package com.mk.spring.demo;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Created by mkanwar on 06/09/17.
@@ -21,5 +24,11 @@ public class BookingAspect {
     public void afterFindingBookings(){
         System.out.println("================================");
         System.out.println("I have found all the bookings!");
+    }
+    @Before("execution(* com.mk.spring.demo.BookingService.book(..)) && args(paramName,..)")
+    public void beforebook(JoinPoint joinPoint, List<String> paramName ){
+        System.out.println("I am about to book following bookings!");
+        paramName.forEach(System.out::println);
+        System.out.println("================================");
     }
 }
