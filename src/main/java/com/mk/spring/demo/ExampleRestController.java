c190281f9adae10bf08d820570404bc0a4d7470d
Mohit Kanwar <m.m.kanwar@gmail.com>
2018-08-23 16:22:14 +0530

    object example

--- src/main/java/com/mk/spring/demo/ExampleRestController.java
+++ src/main/java/com/mk/spring/demo/ExampleRestController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleRestController {
-    @GetMapping("another")
-    public String getAnotherMessage(){
-        return "Another Message";
+    @GetMapping("/user")
+    public User getAnotherMessage(){
+       User user = new User();
+       user.setId(1);
+       user.setName("Newgen");
+       user.setEmail("contactus@newgen.com");
+
+       return user;
     }
 }
